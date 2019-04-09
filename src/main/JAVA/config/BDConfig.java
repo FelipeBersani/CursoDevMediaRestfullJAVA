aa832e0766a7ae039f7af1ef4157d3246315c8b3
FelipeBersani <bersanifelipe@gmail.com>
2019-04-09 17:37:59 -0300

    aulaTravada

---
+++ src/main/JAVA/config/BDConfig.java
@@ -0,0 +1,14 @@
+package main.JAVA.config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class BDConfig {
+
+    public static Connection getConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.jdbc.Driver");
+        return DriverManager.getConnection("jdbc:mysql://localhost:3306/notas_db", "root", "root");
+    }
+
+}
---
+++ src/main/JAVA/rest/NotasService.java
@@ -0,0 +1,47 @@
+package main.JAVA.rest;
+
+import main.JAVA.DAO.NotaDAO;
+import main.JAVA.entidade.Nota;
+
+import javax.annotation.PostConstruct;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Path("/notas")
+public class NotasService {
+
+    private NotaDAO notaDAO;
+
+    @PostConstruct
+    private void init(){
+        notaDAO = new NotaDAO();
+    }
+
+    @GET
+    @Path("/list")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Nota> listarNotas(){
+
+        List<Nota> lista = new ArrayList<>();
+        Nota nota = new Nota();
+        nota.setDescricao("Descricao");
+        nota.setTitulo("Titulo");
+        nota.setId(1);
+        lista.add(nota);
+
+        try {
+//            lista = Optional.of(notaDAO.listarNotas());
+            return lista;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+
+    }
+
+}
